e29d4df334ef9ef93466cbedb535b9f9265c704b
lucas38460 <lmoinel@edenschool.fr>
2023-04-04 17:16:38 +0200

    add style vague

--- src/Views/Hotel/homepage.php
+++ src/Views/Hotel/homepage.php
@@ -3,6 +3,49 @@ ob_start();
 ?>
 
 <section class="homepage">
+    <div id="bubble-wrapper"></div>
+    <style>
+        #bubble-wrapper {
+            z-index: -1;
+            height: 100%;
+            width: 100%;
+
+            position: fixed;
+            bottom: 0px;
+
+            overflow: hidden;
+            /* pointer-events: none; */
+        }
+
+        .bubble {
+            z-index: -1;
+
+            height: max(300px, 30vw);
+            width: max(300px, 30vw);
+
+            background-color: #3f90d3;
+            border-radius: 100%;
+
+            position: absolute;
+            left: 50%;
+            top: 100%;
+
+            animation: wave 2s ease-in-out infinite;
+        }
+
+        @keyframes wave {
+
+            from,
+            to {
+                transform: translate(-50%, 0%);
+            }
+
+            50% {
+                transform: translate(-50%, -20%);
+            }
+        }
+    </style>
+    <!-- PAGE D'INSCRIPTION DU CLIENT -->
     <h2>Que voulez-vous consulter ?</h2>
     <div id="formulaire">
         <form action="/" method="post">
@@ -15,6 +58,23 @@ ob_start();
             <button type="submit">Yep</button>
         </form>
     </div>
+    <script>
+        const wrapper = document.getElementById("bubble-wrapper");
+
+        const animateBubble = (x) => {
+            const bubble = document.createElement("div");
+
+            bubble.className = "bubble";
+
+            bubble.style.left = `${x}px`;
+
+            wrapper.appendChild(bubble);
+
+            setTimeout(() => wrapper.removeChild(bubble), 2000);
+        };
+
+        window.onmousemove = (e) => animateBubble(e.clientX);
+    </script>
 </section>
 
 <?php
